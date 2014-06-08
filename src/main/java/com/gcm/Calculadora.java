ba276f6077825f0f6662779689269e9ed4c9c291
Hivana Macedo <hivanamaced@gmail.com>
2014-06-08 01:56:57 -0300

    add calculadora

---
+++ src/main/java/com/gcm/Calculadora.java
@@ -0,0 +1,20 @@
+package com.gcm;
+
+public class Calculadora {
+	
+	public int soma(int x, int y) {
+		return x+y;
+	}
+	
+	public int subtracao(int x, int y) {
+		return x-y;
+	}
+	
+	public int multiplicacao(int x, int y) {
+		return x*y;
+	}
+	
+	public int divisao(int x, int y) {
+		return x/y;
+	}
+}
